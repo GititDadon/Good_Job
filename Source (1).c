75f0496430ec8d976704615135a665bb0268e003
GititDadon <92985984+GititDadon@users.noreply.github.com>
2021-12-01 11:36:26 +0200

    Print_Function-Yamit Edri

---
+++ Source (1).c
@@ -0,0 +1,39 @@
+#include<stdio.h>
+#include<string.h>
+
+void printf_employee_details(Employee w)// פרטי עובד
+{
+	int n;
+	puts(w.first_name);
+	puts(w.last_name);
+	puts(w.last_name);
+	puts(w.gender);
+	printf("%s",w.id);
+	printf("%d",w.age);
+	puts(w.phone_number);
+	printf("%d",w.password);
+	puts(w.Email);
+	puts(w.living_area);
+	puts(w.Driving_License);
+	puts(w.Education_level);
+	puts(w.Employment_Catagory);
+	printf("%d",w.policies);
+	
+	
+}
+
+
+	if (6 == n)
+	{
+		printf("Goodbye");
+		return 0;
+	}
+
+
+
+	if (7 == n)
+	{
+		printf("Goodbye");
+		return 0;
+	}
+
